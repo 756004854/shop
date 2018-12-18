be27bcec0b07f4badfa425dc5e2a26bf38e91a9a
gaochang1996 <756004854@qq.com>
2018-12-18 22:21:24 +0800

    提交表单验证规则

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="home">
-    <el-form :inline="true" ref="sizeForm" :model="sizeForm" label-width="80px" size="small">
+    <el-form :inline="true" ref="sizeForm" :rules="reles" :model="sizeForm" label-width="80px" size="small">
       <el-form-item  
-        label="姓名"
-        prop="name" 
-        :rules="[
-          { required: true, message: '姓名不能为空' }
-        ]"
+        label="密码"
+        prop="pass"
       >
-        <el-input type="name" v-model="sizeForm.name"></el-input>
+        <el-input type="password" v-model="sizeForm.pass"></el-input>
       </el-form-item>
+      <el-form-item  
+        label="确认密码"
+        prop="checkPass"
+      >
+        <el-input type="password" v-model="sizeForm.checkPass"></el-input>
+      </el-form-item>
+      <br>
       <el-form-item>
         <el-button type="primary" @click="submitForm('sizeForm')">提交</el-button>
         <el-button @click="resetForm('sizeForm')">重置</el-button>
@@ -24,13 +28,37 @@
 export default {
   name: 'home',
   data(){
+    var pass = (rule, value, callback) =>{
+      if( value === "" )
+        callback( new Error("请输入密码"))
+      else
+        if( this.sizeForm.checkPass !== "")
+          this.$refs.sizeForm.validateField("checkPass")
+      callback
+    }
+    var twoPass = (rule, value, callback) => {
+      if( value === "" )
+        callback( new Error("请再次输入密码") )
+      else if( value != this.sizeForm.pass )
+        callback( new Error("两次输入的密码不一致！") )
+      else
+        callback()
+    }
     return {
       sizeForm: {
-        name: ""
+        pass: ""
+      },
+      reles: {
+        pass: [
+          { validator: pass, trigger: 'blur' },
+          { min : 2 , max : 5,  message: `长度在 2 到 5 个字节`}
+        ],
+        checkPass:[
+          { validator: twoPass, trigger: 'blur' },
+        ]
+
       }
     }
-  },
-  components: {
   },
   methods:{
     submitForm(formName) {
