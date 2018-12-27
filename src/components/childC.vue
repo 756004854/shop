8ae72f6c09023119fb198ca177a25fa0fe30121e
gaochang1996 <756004854@qq.com>
2018-12-27 23:09:17 +0800

    提交父子组件传参

---
+++ src/components/childC.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="child">
+        <button @click="clickMe">{{city1}}</button>
+    </div>
+</template>
+<script>
+export default {
+    name: "child",
+    props:{
+        city1:{
+            type: String
+        }
+    },
+    data(){
+        return {
+            city: "河北"
+        }
+    },
+    methods:{
+        clickMe(){
+            this.$emit("clickMe", this.city)
+        }
+    }
+}
+</script>
--- src/views/title/title2.vue
+++ src/views/title/title2.vue
@@ -1,6 +1,26 @@
 <template lang="">
     <div>
-        这是title2
+        这是父组件：{{ city }}
+        <child :city1="city" @clickMe="getChild" ></child>
         <router-view/> 
     </div>
 </template>
+<script>
+import child from "@/components/childC.vue"
+export default {
+    data(){
+        return{
+            city: "北京",
+            isMe: 222,
+        }
+    },
+    components:{
+        child
+    },
+    methods: {
+        getChild(str){
+            this.city = str
+        }
+    }
+}
+</script>
